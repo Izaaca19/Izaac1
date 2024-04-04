7c7f1ef1d9617ae3e7c10774e9735b8977362d24
Izaaca19 <165812253+Izaaca19@users.noreply.github.com>
2024-04-04 07:51:28 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+abstract class Monster {
+ public abstract void attack();
+ public abstract void defend();
+}
+class Godzilla extends Monster{
+ public void attack(){
+  System.out.println("Godzilla attacks with blue blast!");
+ }
+ public void defend(){
+  System.out.println("Godzilla defends with its feet!");
+ }
+}
+class Main{
+  public static void main(String[] args){
+   Godzilla sean = new Godzilla();
+   sean.attack();
+   sean.defend();
+ }
+}
